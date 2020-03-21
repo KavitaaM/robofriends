216050214e50ee0abfd75d3804869f0c170a457a
KavitaaM <platinakavi@gmail.com>
2020-03-21 17:30:19 +0530

    Updates

---
+++ precache-manifest.eec7f4d80b96597ba422ff88232d41d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "960fdaf485eff3b4dc27c4959a21d50e",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7141a545aaa03f60bfad",
+    "url": "/robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "84f8944bffb1125e2241",
+    "url": "/robofriends/static/css/main.0bdd66d2.chunk.css"
+  },
+  {
+    "revision": "7141a545aaa03f60bfad",
+    "url": "/robofriends/static/js/2.1530a8ad.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/robofriends/static/js/2.1530a8ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84f8944bffb1125e2241",
+    "url": "/robofriends/static/js/main.16f16c6a.chunk.js"
+  },
+  {
+    "revision": "92e7806d7355da3f2619",
+    "url": "/robofriends/static/js/runtime-main.a89f6e3b.js"
+  }
+]);
